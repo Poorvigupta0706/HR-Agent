ac67251a00a626bf0b19fb5b96cb552b947c4e03
Poorvigupta0706 <guptapoorvi089@gmail.com>
2026-05-10 13:45:08 -0700

    Remove secrets and cache files

--- config.py
+++ config.py
@@ -1,20 +1,15 @@
 import os
 from dotenv import load_dotenv
-
 from langchain_core.globals import set_llm_cache
 from langchain_community.cache import SQLiteCache
-
 from langchain_google_genai import ChatGoogleGenerativeAI
-
 load_dotenv()
-set_llm_cache(
-    SQLiteCache(database_path=".langchain.db")
-)
+set_llm_cache(SQLiteCache(database_path=".langchain.db"))
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_PROJECT"] = "HR-ATS-System"
 llm = ChatGoogleGenerativeAI(
-    model="gemini-2.0-flash",
+    model="gemini-1.5-flash",
     google_api_key=os.getenv("GOOGLE_API_KEY"),
     temperature=0
 )
-google_api_key = os.getenv("GOOGLE_API_KEY")
-
-langsmith_api_key = os.getenv("LANGCHAIN_API_KEY")
