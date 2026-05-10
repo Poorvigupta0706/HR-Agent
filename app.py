1c356a4d5222990befbaa7586cc848a66aaa8b77
Poorvigupta0706 <guptapoorvi089@gmail.com>
2026-05-10 13:05:22 -0700

    updated changes

---
+++ app.py
@@ -0,0 +1,77 @@
+import streamlit as st
+import fitz  # PyMuPDF
+import docx
+
+from jd_agent import jd_agent
+from resume_agent import resume_agent
+from embeddings_agent import embedding_agent
+from scoring_agent import scoring_agent
+from analysis import analysis_agent
+from interview import interview_agent
+
+
+st.set_page_config(page_title="AI HR ATS System", layout="wide")
+
+st.title("AI HR ATS System")
+
+
+def extract_resume_text(file, file_type):
+    text = ""
+
+    if file_type == "pdf":
+        doc = fitz.open(stream=file.read(), filetype="pdf")
+        for page in doc:
+            text += page.get_text()
+
+    elif file_type == "docx":
+        doc = docx.Document(file)
+        for para in doc.paragraphs:
+            text += para.text + "\n"
+
+    return text
+
+
+jd_text = st.text_area("Paste Job Description")
+
+uploaded_resume = st.file_uploader("Upload Resume", type=["pdf", "docx"])
+
+
+if st.button("Analyze Candidate"):
+
+    if uploaded_resume and jd_text:
+
+        file_type = uploaded_resume.name.split(".")[-1]
+
+        resume_text = extract_resume_text(uploaded_resume, file_type)
+
+        # JD Agent
+        jd = jd_agent(jd_text)
+
+        # Resume Agent
+        resume = resume_agent(resume_text)
+
+        # Embeddings
+        jd_emb = embedding_agent(str(jd))
+        resume_emb = embedding_agent(str(resume))
+
+        # Scoring
+        score = scoring_agent(jd, resume, jd_emb, resume_emb)
+
+        # Analysis
+        analysis = analysis_agent(jd, resume, score)
+
+        # Interview Questions
+        questions = interview_agent(jd, resume)
+
+        # OUTPUT
+        st.subheader("ATS Score")
+        st.write(score)
+
+        st.subheader("Candidate Analysis")
+        st.write(analysis)
+
+        st.subheader("Interview Questions")
+        st.write(questions)
+
+    else:
+        st.warning("Please upload resume and paste JD")
